10b3b1294bc3d61327db1d02bbf6010b904f0e4b
juanda7 <103331036+juanda7@users.noreply.github.com>
2022-04-16 12:50:12 -0600

    Agregamos clase hija

---
+++ ClaseHija.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Globalization;
+
+public class ClaseHija:ClaseBase
+{
+   public double Peso{get;set;}
+
+   public double CalcularPeso(int peso)
+   {
+       return Peso;
+   }
+}
